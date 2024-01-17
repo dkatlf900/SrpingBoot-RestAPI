7723a13c60ea38710ceceaae872a5047c1383e62
jyson <dkatlf900@gmail.com>
2024-01-17 15:56:45 +0900

    first commit

---
+++ src/main/java/com/example/szssonjunyoung/SzsSonjunyoungApplication.java
@@ -0,0 +1,13 @@
+package com.example.szssonjunyoung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SzsSonjunyoungApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SzsSonjunyoungApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/szssonjunyoung/SzsSonjunyoungApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.szssonjunyoung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SzsSonjunyoungApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
